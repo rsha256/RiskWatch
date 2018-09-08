7a2eec5154c3f64ba34ea579c42f0b8f7a11bdc1
Rahul Shah <rahuldshah3@gmail.com>
2018-09-08 02:52:49 -0400

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.gif';
 import './App.css';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem  } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Collapse, NavbarToggler, NavbarBrand, Nav, Navbar, NavItem, NavLink } from 'reactstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faCoffee,
@@ -41,8 +31,11 @@ library.add(
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      modal: false
+      modal: false,
+      collapsed: true
     };
     
     this.toggle = this.toggle.bind(this);
@@ -54,13 +47,37 @@ class App extends Component {
     });
   }
 
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     return (
       <div className="App">
       {/* APP STARTS HERE */}
 
-      <Navbar>
-      <div className="container">
+      {/* <NAVBAR> */}
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">FireWatch</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="https://yeet.yee">Link Daniel Cant see</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/rshah2020/DanielCannotAccessThisSecretRepo">Another Link Daniel Cant see</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+      {/* </NAVBAR> */}
+
+      <div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           
@@ -69,11 +86,10 @@ class App extends Component {
           </h1>
 
 
-
         <div>
           <button color="success" className="btn btn-success" onClick={this.toggle}>
             <FontAwesomeIcon icon={"cloud-upload-alt"} />
-            Upload Image
+            &nbsp;&nbsp;Upload Image
           </button>
 
           {/* <MODAL> */}
@@ -84,7 +100,7 @@ class App extends Component {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </ModalBody>
             <ModalFooter>
-              <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+              <Button color="primary" onClick={this.toggle}>Browse</Button>{' '}
               <Button color="secondary" onClick={this.toggle}>Cancel</Button>
             </ModalFooter>
           </Modal>
