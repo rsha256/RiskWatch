512a1054d38eb13a728b664601f8375e02e5dcb5
Daniel Wang <hello@danielwang.dev>
2018-09-08 17:37:46 -0400

    Remove service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Map from "./Map";
-import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<Map />, document.getElementById("map"));
-registerServiceWorker();
