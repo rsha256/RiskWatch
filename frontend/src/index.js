09fe3d15125d6cf2d74e46c17cab257a1b940fda
Daniel Wang <hello@danielwang.me>
2018-09-08 15:18:51 -0400

    Add a map into the site

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Map from "./Map";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Map />, document.getElementById("map"));
 registerServiceWorker();
