83de49e4ab348ccaa6cbbd8d2edae29d1fe73a5e
Daniel Wang <hello@danielwang.me>
2018-09-08 08:01:23 -0400

    Add early stage of object detection code

---
+++ flaskstructure/object_recognition.py
@@ -0,0 +1,23 @@
+import boto3
+
+
+def convertImageToBytes(imageFileName):
+    with open(imageFileName, 'rb') as image_file:
+        return image_file.read()
+
+
+def getLabels(imageFileName):
+    client = boto3.client('rekognition')
+
+    response = client.detect_labels(
+        Image={
+            'Bytes': convertImageToBytes(imageFileName)
+        },
+        MaxLabels=123
+    )
+
+    print(response)
+
+
+if __name__ == "__main__":
+    getLabels("example.png")
