3a79e43992f0cb9f2e14ad445417f0c0e8dd9a38
Rahul Shah <rahuldshah3@gmail.com>
2018-09-08 05:39:13 -0400

    pull not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,7 +97,11 @@ class App extends Component {
           <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
             <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
             <ModalBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <form action="" method="post" enctype="multipart/form-data">
+              Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload" />
+                <input type="submit" value="Upload Image" name="submit" />
+              </form>
             </ModalBody>
             <ModalFooter>
               <Button color="primary" onClick={this.toggle}>Browse</Button>{' '}
