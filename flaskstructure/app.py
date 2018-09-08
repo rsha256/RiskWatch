a3d1d1b7d3d2a25d600bd80c21e656d64811833f
Daniel Wang <hello@danielwang.me>
2018-09-08 08:43:40 -0400

    Store files in /var/www/images

--- flaskstructure/app.py
+++ flaskstructure/app.py
@@ -22,8 +22,7 @@ def addRisk():
     image = request.files['image']
     filename = str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month) + '-' + str(datetime.datetime.now().day) + '-' + str(
         datetime.datetime.now().hour) + '-' + str(datetime.datetime.now().minute) + '-' + str(datetime.datetime.now().second) + '-' + str(datetime.datetime.now().microsecond)
-    image.save(filename + '.' + imghdr.what(image))
-    return(redirect('http://localhost:3000'))
+    image.save('/var/www/images' + filename)
     # database.addRisk(filename, data['location'], data['risktype'])
 
 
