5415f398dfa57d560a054e8cda79bafe09def533
Rahul Shah <rahuldshah3@gmail.com>
2018-09-08 17:18:58 -0400

    tmp stash

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
+// FAB Stuff
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import { faAlignRight } from "@fortawesome/free-solid-svg-icons";
+
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>Hello World</h1>
-          </div>
-        </InfoWindow>
-      </Map>
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Hello World</h1>
+            </div>
+          </InfoWindow>
+          <Button variant="fab" color="secondary" aria-label="Add" className={faAlignRight}>
+            <AddIcon />
+          </Button>
+        </Map>
     );
   }
 }
